cfed1792942b03b5dd18c41041116bac4fab3020
Martin <martohaiek@gmail.com>
2015-04-26 22:46:59 +0200

    web app
    meterlo adento de la app

---
+++ web-app-martin/js/app.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/* App Module */
+
+//angular.module('bloodWindow', ['ui.router', 'bloodWindowAnimations', 'bloodWindowControllers', 'bloodWindowServices' ])
+
+angular.module('construirTV', ['ngRoute', 'ui.bootstrap', 'construirTVControllers' ])
+
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: 'templates/content.home.html',
+          controller: 'HomeCtrl'
+        }).
+        when('/titles', {
+          templateUrl: 'templates/content.titulos.html',
+          controller: 'TitulosCtrl',
+          resolve: {
+            validateToken: ['$http','$rootScope', function($http, $rootScope) {
+              $rootScope.validateToken();
+            }]
+          }
+        }).
+        when('/titles/:tituloId', {
+          templateUrl: 'templates/content.titulos.detail.html',
+          controller: 'TitulosDetailCtrl',
+          resolve: {
+            validateToken: ['$http','$rootScope', function($http, $rootScope) {
+              $rootScope.validateToken();
+            }]
+          }
+        }).
+        when('/register', {
+          templateUrl: 'templates/content.registro.html',
+          controller: 'RegistroCtrl'
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+
+        /*
+        // ELIMINATE "#" FROM URL. Check browser support
+        if(window.history && window.history.pushState){
+            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+         // if you don't wish to set base URL then use this
+         $locationProvider.html5Mode({
+                 enabled: true,
+                 requireBase: false
+          });
+        }
+        */
+    }])
+
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+      
+      // Declare global variables to acces to API
+      $rootScope.client_id = "2_3yhgp61uxcys4gs0sw4oos008c0wkwokkw4g0k0044sk8kso0k";
+      $rootScope.client_secret = "1frw8e9jdcw08kcg844k8s8cksowk48gogo848soo4kkowc88k";
+
+      // Declare user name
+      $rootScope.userName = "";
+
+      //if($location.path())
+      if($location.host() == "bloodwindow.tv") { // For production
+        $rootScope.serverURL = "/web"; // URL for real app
+        $rootScope.imagesSrc = "/uploads";
+      } else { // For development
+        $rootScope.serverURL = "/web/app_dev.php"; // URL for working local
+        $rootScope.imagesSrc = "/uploads";
+      }
+    }]);
---
+++ web-app-martin/js/controllers.js
@@ -0,0 +1,395 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('construirTVControllers', [])
+
+.controller('HeaderCtrl', ['$scope', '$http', '$rootScope', '$modal', '$location', '$routeParams', function($scope, $http, $rootScope, $modal, $location, $routeParams) {
+  
+  // Set the value to variable for updating class active in header menu
+  $rootScope.currentUrl = $location.path();
+  $scope.isCollapsed = true; // Bootstrap header setting
+   
+  $rootScope.searchInputText = ""; // Set var to emty string, because if not is 'undefined'
+  $scope.change = function () {
+    if (angular.isUndefined($scope.searchInput) || $scope.searchInput == null) {
+      $rootScope.searchInputText = "";
+    }
+    else {
+      $rootScope.searchInputText = $scope.searchInput;
+    }
+  }
+
+  // ***** START OPEN MODAL LOGIN *****
+  $scope.openLogin = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/modals/modal.login.html',
+      controller: 'LoginCtrl',
+      size: 'lg',
+      backdrop: 'static',
+      keyboard: false
+    });
+
+  };
+  // ***** END OPEN MODAL LOGIN *****
+
+  // ***** LOOK IN LOCAL STORAGE FOR VALID TOKEN *****
+  $rootScope.validateToken = function() {
+    // Look in local storage for token
+    if(window.localStorage['access_token']) {
+      // ***** START API ***** Validate token
+      $http({
+        method: 'GET',
+        //url: $rootScope.serverURL + "/oauth/v2/token?client_id=" + $rootScope.client_id + "&client_secret=" + $rootScope.client_secret + "&grant_type=password&username=" + ajam28d + "&password=" + 123;
+        url: $rootScope.serverURL + "/api/articles?access_token=" + window.localStorage['access_token']
+        })
+        .success(function(data, status){
+            $rootScope.userName = data; // CHECK THIS
+            console.log(data, status);  //remove for production
+        })
+        .error(function(data, status){
+            $scope.openLogin();
+            console.log(data, status); //remove for production
+      });
+      // ***** END API *****
+    } else { // If there is no token in logal storage open login
+      $scope.openLogin();
+    }
+  }
+  // ***** END LOOK IN LOCAL STORAGE FOR VALID TOKEN *****
+
+  // ***** LOGOUT USER *****
+  $scope.userLogout = function() {
+    window.localStorage['access_token'] = "";
+  }
+  // ***** END LOGOUT USER *****
+
+
+}])
+
+.controller('languageCtrl', ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+  
+  $rootScope.language = "eng"; // Set default language to Spanish
+
+  // ***** SET LENGUAGUE *****
+  $scope.setLanguage = function(thisLanguage) {
+    $rootScope.language = thisLanguage;
+    $http({
+    method: 'GET',
+    url: "languages/" + thisLanguage + ".json"
+    })
+    .success(function(data, status){
+        $rootScope.languageHeader = data.header; // All tags used in SidebarCtrl
+        $rootScope.languageLogIn = data.login; // All tags used in LoginCtrl
+        $rootScope.languageHome = data.home; // All tags used in HomeCtrl
+        $rootScope.languageTituloDetail = data.tituloDetail; // All tags used in TituloDetailCtrl
+        $rootScope.languageRegister = data.register; // All tags used in RegistroCtrl
+
+        console.log(data, status);  //remove for production
+    })
+    .error(function(data, status){
+        console.log(data, status); //remove for production
+    });
+  }
+  // ***** END SET LENGUAGUE *****
+
+  $scope.setLanguage("esp"); // Set default language to Spanish
+  
+}])
+
+.controller('LoginCtrl', ['$scope', '$routeParams', '$http', '$rootScope', '$modalInstance', '$location', '$sce', function($scope, $routeParams, $http, $rootScope, $modalInstance, $location, $sce) {
+    
+  // ***** START API ***** Get corto detail
+  $scope.showPreloader = true; // Show preloader gif
+  $scope.cortoResult = true; // Set to true for showing label titles in pop up
+
+  // ***** START MODAL CLOSE FUNCTIONS *****
+  $scope.ok = function () {
+    $location.path("/").replace(); // If user quits login, redirect to home
+    $modalInstance.close(); // Close modal after redirecting
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+  // ***** END MODAL CLOSE FUNCTIONS *****
+
+
+  // ***** LOGIN USER *****
+  $scope.userLogin = function(email, password) {
+      // ***** START API ***** Get token
+      $http({
+        method: 'GET',
+        url: $rootScope.serverURL + "/oauth/v2/token?client_id=" + $rootScope.client_id + "&client_secret=" + $rootScope.client_secret + "&grant_type=password&username=" + email + "&password=" + password
+        })
+        .success(function(data, status){
+            $rootScope.userName = data.user_name; // CHECK THIS
+            // Save token in local storage
+            window.localStorage['access_token'] = data.access_token; //CHECK THIS
+            //window.localStorage['access_token'] = "YzZiYjhmNmIxYTcyMTY5MTU3MGQyYTJkZGNkMzM4N2JkZWZhNTBjYWYxNjA2MDM1NWNkMDcxMjYyM2VlYTNjNw";
+            console.log(data, status);  //remove for production
+        })
+        .error(function(data, status){
+            $scope.showErrors = data; //CHECK THIS
+            console.log(data, status); //remove for production
+      });
+      // ***** END API *****
+  }
+  // ***** END LOGIN USER *****
+
+}])
+
+// **************************************** HASTA ACA LLEGUE ****************************************
+// TODO:
+// * Guardar Usuario en local storage cuando se loguea o conecta
+// * 
+
+.controller('HomeCtrl', ['$scope', '$http', '$rootScope', '$modal', '$location', '$routeParams', function($scope, $http, $rootScope, $modal, $location, $routeParams) {
+  // Set the value to variable for updating class active in header menu
+  // In this case harcode the URL for showing active HOME in menu, besides the cortoID (if any)
+  $rootScope.currentUrl = "/";
+  window.scrollTo(0,0);
+
+  // ***** START API ***** Get Carousel Cortos
+  $scope.slides = "";
+  $scope.getCarouselCortos = function() {
+    $http({
+    method: 'POST',
+    url: $rootScope.serverURL + "/buscar/corto/carousel"
+    })
+    .success(function(data, status){
+        $scope.slides = data;
+        console.log(data, status);  //remove for production
+    })
+    .error(function(data, status){
+        console.log(data, status); //remove for production
+    });
+  }
+  // ***** END API *****
+
+  // ***** START API ***** Get All Cortos with filters
+  $scope.filterCortos = true; // Set var to true for not showing error message
+  $scope.getFilterCortos = function() {
+    $scope.showPreloader = true; // Show preloader gif
+    $scope.searchGenero = ""; // Edit stored parameters
+    $scope.sendToAPI = '{"inputBuscador": "' + $scope.searchBuscador + '", "genero": "' + $scope.searchGenero + '", "festival": "' + $scope.searchFestival + '"}'; // inputBuscador take the value of 'titulo', 'anio' and 'director'
+    $http({
+        method: 'POST',
+        url: $rootScope.serverURL + "/buscar/corto",
+        data: $scope.sendToAPI
+    })
+    .success(function(data, status){
+        $scope.filterCortos = data;
+        $scope.showPreloader = false; // Hide preloader gif
+        console.log(data, status);  //remove for production
+    })
+    .error(function(data, status){
+        $scope.filterCortos = ""; // Set var to lenght cero for showing error message
+        $scope.showPreloader = false; // Hide preloader gif
+        console.log(data, status); //remove for production
+    });
+  }
+  // ***** END API *****
+
+  // ***** START API ***** Get all Festivals for filter
+  $scope.festivals = "";
+  $scope.getAllFestivals = function() {
+    $http({
+    method: 'POST',
+    url: $rootScope.serverURL + "/buscar/festival"
+    })
+    .success(function(data, status){
+        $scope.festivals = data;
+        console.log(data, status);  //remove for production
+    })
+    .error(function(data, status){
+        console.log(data, status); //remove for production
+    });
+  }
+  // ***** END API *****
+
+  // ***** START CAROUSEL *****
+  $scope.getCarouselCortos();
+  $scope.myInterval = 5000;
+
+  // ***** END CAROUSEL *****
+
+  // ***** START INPUT SEARCH RESULT *****
+  // Get value from input search and get data from API
+  $scope.showCarousel = true;
+  $scope.$watch('searchInputText', function() {
+    window.scrollTo(0,0);
+    if($rootScope.searchInputText) $scope.showCarousel = false;
+    else $scope.showCarousel = true;
+    //$scope.searchFestival = ""; uncoment for reseting festivals to all
+    $scope.searchBuscador = $rootScope.searchInputText;
+    $scope.getFilterCortos(); // This function will be call when controller is called and when $rootScope.searchInputText changes.
+
+  });
+  // ***** END INPUT SEARCH RESULT *****
+
+  // ***** START RESULT TABS *****
+  $scope.searchFestival = "";
+  $scope.searchBuscador = "";
+  $scope.getAllFestivals();
+  $scope.setCurrentFestival = function(value) {
+    $scope.searchFestival = value;
+    $scope.getFilterCortos();
+  }
+  // ***** END RESULT TABS *****
+
+  // ***** START SHOW PARAMETER CORTO ID DETAIL *****
+  $scope.id = $routeParams.cortoId;
+  if($scope.id) {
+    $scope.openCortoDetail($scope.id);
+  }
+  // ***** END SHOW PARAMETER CORTO ID DETAIL *****
+
+}])
+
+.controller('CortoDetailCtrl', ['$scope', '$routeParams', '$http', '$rootScope', '$modalInstance', '$location', 'cortoId', '$sce', function($scope, $routeParams, $http, $rootScope, $modalInstance, $location, cortoId, $sce) {
+
+  $scope.id = cortoId; // cortoId value is set in the function 'openCortoDetail' located in 'HomeCtrl'
+  
+  //$location.path("/home/" + $scope.id).replace(); // Change URL to HOME for deleting crotId parameter (if exists) 
+  
+  // ***** START API ***** Get corto detail
+  $scope.showPreloader = true; // Show preloader gif
+  $scope.cortoResult = true; // Set to true for showing label titles in pop up
+  $scope.sendToAPI = '{"id": "' + $scope.id + '" }';
+  $http({
+      method: 'POST',
+      url: $rootScope.serverURL + "/corto",
+      data: $scope.sendToAPI
+  })
+  .success(function(data, status){
+      $scope.cortoResult = data[0];
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status);  //remove for production
+  })
+  .error(function(data, status){
+      $scope.cortoResult = false; // Set to false for showing error pop up
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status); //remove for production
+  });
+  // ***** END API *****
+
+  // ***** START MODAL CLOSE FUNCTIONS *****
+  $scope.ok = function () {
+    $modalInstance.close();
+    $location.path("/home").replace(); // Change URL to HOME for deleting crotId parameter (if exists)
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+  // ***** END MODAL CLOSE FUNCTIONS *****
+
+  // ***** START VIMEP VALIDATION URL *****
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl("//player.vimeo.com/video/" + src);
+  }
+  // ***** END VIMEP VALIDATION URL *****
+
+}])
+
+.controller('TitulosCtrl', ['$scope', '$http', '$rootScope', '$location', function($scope, $http, $rootScope, $location) {
+  // Set the value to variable for updating class active in header menu
+  $rootScope.currentUrl = $location.path();
+  window.scrollTo(0,0);
+
+  // ***** START API ***** Get All Proyectos with filters
+  $scope.orderBy = "anio"; // Edit stored parameters
+  $scope.orderBy = "financiacion"; // Edit stored parameters
+
+  $scope.getAllProyectos = function() {
+    $scope.sendToAPI = '{"orderBy": "' + $scope.orderBy + '"}';
+    $http({
+        method: 'POST',
+        url: $rootScope.serverURL + "/industria/proyectos",
+        data: $scope.sendToAPI
+    })
+    .success(function(data, status){
+
+        console.log(data, status);  //remove for production
+        
+        // Split the results in arrays of 4 elements
+        $scope.proyectos = [];
+        while (data.length > 0)
+        $scope.proyectos.push(data.splice(0, 4));
+        
+    })
+    .error(function(data, status){
+        console.log(data, status); //remove for production
+    });
+  }
+  // ***** END API *****
+  $scope.getAllProyectos();
+}])
+
+.controller('TitulosDetailCtrl', ['$scope', '$routeParams', '$http', '$rootScope', '$location', function($scope, $routeParams, $http, $rootScope, $location) {
+
+  // Set the value to variable for updating class active in header menu
+  // In this case harcode the URL for showing active PROYECTOS in menu
+  $rootScope.currentUrl = "/proyectos";
+  window.scrollTo(0,0);
+
+  $scope.id = $routeParams.proyectoId;
+
+  // ***** START API ***** Get proyecto detail
+  $scope.showPreloader = true; // Show preloader gif
+  $scope.proyectoResult = true; // Set to true for showing label titles
+  $scope.sendToAPI = '{"id": "' + $scope.id + '" }';
+  $http({
+      method: 'POST',
+      url: $rootScope.serverURL + "/industria/proyecto",
+      data: $scope.sendToAPI
+  })
+  .success(function(data, status){
+      $scope.proyectoResult = data[0];
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status);  //remove for production
+
+  })
+  .error(function(data, status){
+      $scope.proyectoResult = false; // Set to false for showing error pop up
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status); //remove for production
+  });
+  // ***** END API *****
+
+}])
+
+.controller('RegistroCtrl', ['$scope', '$routeParams', '$http', '$rootScope', '$location', function($scope, $routeParams, $http, $rootScope, $location) {
+
+  // Set the value to variable for updating class active in header menu
+  // In this case harcode the URL for showing active PROYECTOS in menu
+  $rootScope.currentUrl = "/proyectos";
+  window.scrollTo(0,0);
+
+  $scope.id = $routeParams.proyectoId;
+
+  // ***** START API ***** Get proyecto detail
+  $scope.showPreloader = true; // Show preloader gif
+  $scope.proyectoResult = true; // Set to true for showing label titles
+  $scope.sendToAPI = '{"id": "' + $scope.id + '" }';
+  $http({
+      method: 'POST',
+      url: $rootScope.serverURL + "/industria/proyecto",
+      data: $scope.sendToAPI
+  })
+  .success(function(data, status){
+      $scope.proyectoResult = data[0];
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status);  //remove for production
+
+  })
+  .error(function(data, status){
+      $scope.proyectoResult = false; // Set to false for showing error pop up
+      $scope.showPreloader = false; // Hide preloader gif
+      console.log(data, status); //remove for production
+  });
+  // ***** END API *****
+
+}])
